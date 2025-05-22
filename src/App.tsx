fcb4f6c9a9a3d432bf9bfa04ff06312b51bda77a
Marina-Portillo <maryypucca@gmail.com>
2025-05-21 20:38:11 -0600

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
     <div>
-    <h1 className="font-extrabold text-2xl">Innova System</h1>
+      <h1 className="font-extrabold text-2xl">Innova System</h1>
+
+      <footer>
+        <p>&copy; 2025 Innova System. Todos los derechos reservados.</p>
+        <p>
+          <a href="mailto:innovaSystem@company.com">Contacto</a>
+        </p>
+      </footer>
     </div>
-  )
+  );
 }
